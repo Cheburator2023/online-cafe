368eee012327e35b6ecf20528c14e3da8fa1c543
Victor Gabbasov <borisborisov03102001@gmail.com>
2025-11-05 18:47:16 +0300

    feat: Поправил Menu-Service

--- menu-service/src/test/java/ru/otus/cafe/menu/MenuApplicationTests.java
+++ menu-service/src/test/java/ru/otus/cafe/menu/MenuApplicationTests.java
@@ -3,11 +3,34 @@ package ru.otus.cafe.menu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class MenuApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("test_menu_db")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.flyway.enabled", () -> "true");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "validate");
+    }
 
     @Test
     void contextLoads(ApplicationContext context) {
